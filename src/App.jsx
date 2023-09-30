a1d3d368fb343f8796c1f9f0f284b4910d58d4b1
Laudier2 <trabalhandooline2019@hotmail.com>
2023-09-30 00:46:40 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import Products from "./components/Products"
+import NavBar from "./components/NavBar"
 
 export default function App() {
   return (
-    <div className="container p-5">
+    <div>
+      <NavBar />
       <Products />
     </div>
   )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -5,23 +5,26 @@ export default function Products() {
     const product = useSelector(productsSlice => productsSlice.products.items)
 
     return (
-        <div className="card-deck">
-            <h2>Produtos Dispaonivel</h2>
-            <div className="card col-4">
-                {product.map(r => (
-                    <>
-                        <div key={r.id} className="card p-1">
-                            <img className="card-img-top" src={r.image} alt="Card image cap" />
-                            <div className="card-body">
-                                <h5 className="card-title">{r.name}</h5>
-                                <p className="card-text">r.description</p>
+        <div className="container">
+            <div className="card-deck">
+                <h2>Produtos Dispaonivel</h2>
+                <div className="card-group col-5">
+                    {product.map(r => (
+                        <>
+                            <div key={r.id} className="card p-1">
+                                <img className="card-img-top" src={r.image} alt="Card image cap" />
+                                <div className="card-body">
+                                    <h5 className="card-title">{r.name}</h5>
+                                    <p className="card-text">{r.description}</p>
+                                </div>
+                                <div className="card-footer">
+                                    <button className="text-muted bg-info btn font-weight-bold m-1">Comprar</button>
+                                    <button className="text-muted bg-info btn font-weight-bold">Add Cart</button>
+                                </div>
                             </div>
-                            <div className="card-footer  bg-info">
-                                <small className="text-muted">Ver mais detalhes</small>
-                            </div>
-                        </div>
-                    </>
-                ))}
+                        </>
+                    ))}
+                </div>
             </div>
         </div>
     )
